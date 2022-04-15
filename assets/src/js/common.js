8b5d26cd9d3f597dc8b4b88fd186d7987565dd71
Daniil <boiko.81@list.ru>
2022-04-15 12:24:13 +0300

    update textarea in form

--- assets/src/js/common.js
+++ assets/src/js/common.js
@@ -7,14 +7,12 @@ import mobileMenu from './components/mobile-menu';
 import youtubeIframe from './components/youtube-iframe';
 import sliders from './components/sliders';
 import transfer from './components/transfer';
-import textarea from './components/textarea';
 
 document.addEventListener("DOMContentLoaded", () => {
     mobileMenu();
     youtubeIframe();
     transfer();
     sliders();
-    textarea();
 
     AOS.init({
         duration: 500,
--- assets/src/js/components/textarea.js
+++
@@ -1,6 +0,0 @@
-export default function(){
-    const textareaAutosize = document.querySelector('.textarea-autosize textarea');
-    textareaAutosize && textareaAutosize.addEventListener('input', e => {
-        textareaAutosize.parentNode.dataset.replicatedValue = textareaAutosize.value;
-    })
-}
