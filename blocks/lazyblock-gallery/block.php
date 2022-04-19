1e9f2c9cf1c018189624a6ed1acceb9c715a08f6
Daniil <boiko.81@list.ru>
2022-04-19 11:59:25 +0300

    gallery block

---
+++ blocks/lazyblock-gallery/block.php
@@ -0,0 +1,18 @@
+<section class="gallery">
+    <div class="container">
+
+        <?php if(!empty($attributes['title'])): ?>
+            <h1 class="page-title"><?php echo $attributes['title'] ?></h1>
+        <?php endif; ?>
+
+        <div class="gallery-grid">
+
+            <?php foreach( $attributes['images'] as $image ): ?>
+                <a href="<?php echo $image['url']?>" data-fancybox="gallery" class="gallery-grid-item">
+                    <img src="<?php echo $image['url']?>" alt="<?php echo $image['alt']?>">
+                </a>
+            <?php endforeach; ?>
+
+        </div>
+    </div>
+</section>
