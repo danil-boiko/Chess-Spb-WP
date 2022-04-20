b722405113889117f4bc4f090d96fd14a40abbfd
Daniil <boiko.81@list.ru>
2022-04-20 12:12:43 +0300

    shedule page

---
+++ shedule.php
@@ -0,0 +1,12 @@
+<?php
+/* Template Name: Страница расписания */
+get_header();
+?>
+<div class="container shedule-title">
+    <h1 class="page-title"><?php single_post_title() ?></h1>
+    <p>Расписание очных занятий с 15 августа 2021</p>
+</div>
+<?php the_content()?>
+	
+<?php
+get_footer();
