9be924d89d8cd960dd0a4570802397b717a6e61e
Daniil <boiko.81@list.ru>
2022-04-28 11:55:35 +0300

    category page

--- archive.php
+++ archive.php
@@ -1,37 +1,41 @@
 <?php
 get_header();
 ?>
-        <div class="blog">
-        <div class="blog__title">
+    <div class="blog__posts">
+        <div class="blog__posts-title"><?php single_term_title() ?></div>
             <div class="container">
-                <h1 class="page-title"><?php single_post_title() ?></h1>
+                <div class="blog__posts-grid">
+                    <?php while(have_posts()): the_post();?>
+                        <div class="blog__posts-grid-item">
+                            <a class="blog__posts-grid-item_image" href="<?php echo get_permalink(); ?>">
+                                <?php echo get_the_post_thumbnail() ?>
+                            </a>
+                            <div class="blog__posts-grid-item_info">
+                                <ul class="blog__posts-grid-item_info-categories">
+                                    <?php foreach( get_the_category() as $category ): ?>
+                                        <li>
+                                            <a href="<?php echo get_category_link($category) ?>">
+                                                <?php echo $category->name ?>
+                                            </a>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                                <div class="blog__posts-grid-item_info-time">
+                                    <?php echo reading_time(get_the_ID()); ?>
+                                </div>
+                            </div>
+                            <div class="blog__posts-grid-item_title">
+                                <?php the_title() ?>
+                            </div>
+                            <p class="blog__posts-grid-item_excerpt">
+                                <?php echo get_the_excerpt() ?>
+                            </p>
+                            <a class="blog__posts-grid-item_link" href="<?php echo get_permalink(); ?>">Перейти</a>
+                        </div>  
+                    <?php endwhile; ?>
+                 </div>
             </div>
-        </div>
-
-        <div class="blog__posts">
-            <div class="container">
-                <?php
-                
-                // $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1; // определяем текущую страницу блога
-                // $args = array(
-                //     'posts_per_page' => get_option('posts_per_page'), // значение по умолчанию берётся из настроек, но вы можете использовать и собственное
-                //     'paged'          => $current_page // текущая страница
-                // );
-                // query_posts( $args );
-                
-                // $wp_query->is_archive = true;
-                // $wp_query->is_home = false;
-                
-                // while(have_posts()): the_post();
-                //     ?>
-                     <h2><?php// the_title() /* заголовок */ ?></h2>
-                     <p></p>
-                     <?php
-                // endwhile;
-            
-                // ?>
-            </div>
-        </div>
+        <div>
     </div>
 	
 <?php
